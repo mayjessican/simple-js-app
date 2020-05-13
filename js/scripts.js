5c78c5e8c446cc3029149a575ade5113e6a747da
mayjessican <63651624+mayjessican@users.noreply.github.com>
2020-05-13 16:12:26 +0200

    edited for task 6

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//IIFE wrap
 var pokemonRepository = (function () {
   var repository = [{
     creatureName: 'Ninetales',
@@ -24,6 +25,7 @@ var pokemonRepository = (function () {
     creatureTypes: ['Sleep', 'Charm'],
   }];
 
+//function to push pokemon repository
 function add(pokemon) {
       repository.push(pokemon);
 }
@@ -32,21 +34,41 @@ function getAll() {
       return repository;
 }
 
-return {
-      add: add,
-      getAll: getAll
+return{
+  add: add,
+  getAll: getAll,
+  addListItem: addListItem,
+  loadList: loadList,
+  loadDetails: loadDetails,
+  createReusableModal: createReusableModal,
+  showModal: showModal,
+  hideModal: hideModal
 };
-})();
 
+//Function to add list for each pokemon object
+function addListItem(pokemon) {
+  var listItem = document.createElement('li');
+  var button = document.createElement('button');
+  button.innerText = pokemon.creatuerName;
+  button.classList.add('pokemon-name');
+  listItem.appendChild(button);
+  $pokemonList.appendChild(listItem)
+  button.addEventListener('click', function() {
+    showDetails(pokemon)
+  })
+}
+
+//Function to add another element
 console.log(pokemonRepository.getAll());
 pokemonRepository.add({ creatureName: 'Pikachu' });
 console.log(pokemonRepository.getAll());
 
-pokemonRepository.getAll().forEach (function(pokemon){
-  console.log('<h2>' + pokemon.creatureName + '</h2>' + 'Height:' + pokemon.creatureHeight);
-  if (pokemon.creatureHeight >= 2.00){
-    document.write('<h2>' + pokemon.creatureName + '</h2>' + 'Height:' + pokemon.creatureHeight + '(Wow that\'s big)!')
-  } else {
-    document.write('<h2>' + pokemon.creatureName + '</h2>' + 'Height:' + pokemon.creatureHeight);
-  }
-}); 
+//Creates list of Pokemon with Pokemon's name on the button
+pokemonRepository.loadList().then(function() {
+  //Creates a reusable modal once
+pokemonRepository.createReusableModal();
+  //Data loaded
+pokemonRepository.getAll().forEach(function(pokemon){
+pokemonRepository.addListItem(pokemon);
+  });
+});
