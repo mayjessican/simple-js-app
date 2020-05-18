72e4001810abcbc327336d968ce23b4163839086
mayjessican <63651624+mayjessican@users.noreply.github.com>
2020-05-18 15:00:03 +0200

    promise-polyfill

--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,77 @@
-//IIFE wrap
-var pokemonRepository = (function () {
-  var repository = [{
-    creatureName: 'Ninetales',
-    creatureHeight: 1.1,
-    creatureAbilities: ['Flash-fire', 'Drought'],
-    creatureTypes: ['Field'],
-  },
-  {
-    creatureName: 'Mew',
-    creatureHeight: 0.4,
-    creatureAbilities: ['Synchronize'],
-    creatureTypes: ['Cat'],
-  },
-  {
-    creatureName: 'Charmander',
-    creatureHeight: 2.00,
-    creatureAbilities: ['Blaze', 'Solar-power'],
-    creatureTypes: ['Fire'],
-  },
-  {
-    creatureName: 'Jigglypuff',
-    creatureHeight: 0.5,
-    creatureAbilities: ['Cute-charm', 'Friend-guard'],
-    creatureTypes: ['Sleep', 'Charm'],
-  }];
+//IIFE
+var pokemonRepository = (function(pokemon) {
+    var pokemonList = [];
+    var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
+    function loadList() {
+        return fetch(apiUrl).then(function(response) {
+            return response.json();
+        }).then(function(json) {
+            json.results.forEach(function(item) {
+                var pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+            });
+        }).catch(function(e) {
+            console.error(e);
+        })
+    }
+})();
+
+pokemonRepository.loadList().then(function() {
+    // Now the data is loaded!
+    pokemonRepository.getAll().forEach(function(pokemon) {
+        addListItem(pokemon);
+    });
+
+    return {
+        add: add,
+        getAll: getAll,
+        addListItem: addListItem,
+        loadList: loadList,
+        loadDetails: loadDetails,
+        createReusableModal: createReusableModal,
+        showModal: showModal,
+        hideModal: hideModal
+    };
+
+});
 
 //function to push pokemon repository
 function add(pokemon) {
-      repository.push(pokemon);
+    pokemnonRepository.push(pokemon);
 }
 
 function getAll() {
-      return repository;
+    return pokemonRepository;
 }
-
-return{
-  add: add,
-  getAll: getAll,
-  addListItem: addListItem,
-  loadList: loadList,
-  loadDetails: loadDetails,
-  createReusableModal: createReusableModal,
-  showModal: showModal,
-  hideModal: hideModal
-};
 
 //Function to add list for each pokemon object
 function addListItem(pokemon) {
-  var listItem = document.createElement('li');
-  var button = document.createElement('button');
-  button.innerText = pokemon.creatuerName;
-  button.classList.add('pokemon-name');
-  listItem.appendChild(button);
-  $pokemonList.appendChild(listItem)
-  button.addEventListener('click', function() {
-    showDetails(pokemon)
-  })
-}
+    var listItem = document.createElement('li');
+    var button = document.createElement('button');
+    button.innerText = pokemon.creatuerName;
+    button.classList.add('pokemon-name');
+    listItem.appendChild(button);
+    $pokemonList.appendChild(listItem)
+    button.addEventListener('click', function() {
+        showDetails(pokemon)
+    })
 
-//Function to add another element
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({ creatureName: 'Pikachu' });
-console.log(pokemonRepository.getAll());
+    //Function to add another element
+    console.log(pokemonRepository.getAll());
+    pokemonRepository.add({
+        creatureName: 'Pikachu'
+    });
+    console.log(pokemonRepository.getAll());
 
-//Creates list of Pokemon with Pokemon's name on the button
-pokemonRepository.loadList().then(function() {
-  //Creates a reusable modal once
-pokemonRepository.createReusableModal();
-  //Data loaded
-pokemonRepository.getAll().forEach(function(pokemon){
-pokemonRepository.addListItem(pokemon);
-  });
-});
+    //Creates list of Pokemon with Pokemon's name on the button
+    pokemonRepository.loadList().then(function() {
+                //Creates a reusable modal once
+                pokemonRepository.createReusableModal();
+                //Data loaded
+                pokemonRepository.getAll().forEach(function(pokemon) {
+                    pokemonRepository.addListItem(pokemon);
+    });
